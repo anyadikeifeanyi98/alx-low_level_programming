cedc1e3771168f20fe1e9915e7202561938e82e0
Anyadike Ifeanyi <ianyadike98@gmail.com>
2023-03-24 23:58:27 +0000

    second commit

---
+++ 0x04-more_functions_nested_loops/0-isuppet.c
@@ -0,0 +1,30 @@
+ program to check whether a character is uppercase 
+  * or lowercase using inbuilt library functions
+  *  */
+
+#include <stdio.h>
+#include <ctype.h> /* Used for isupper() and islower() */
+
+  int main()
+{
+	    char ch;
+
+	        /* Input character from user */
+	        printf("Enter any character: ");
+		    scanf("%c", &ch);
+
+		        if(isupper(ch))
+				    {
+					            printf("'%c' is uppercase alphabet.", ch);
+						        }
+			    else if(islower(ch))
+				        {
+						        printf("'%c' is lowercase alphabet.", ch);
+							    }
+			        else
+					    {
+						            printf("'%c' is not an alphabet.", ch);
+							        }
+
+				    return 0;
+}
