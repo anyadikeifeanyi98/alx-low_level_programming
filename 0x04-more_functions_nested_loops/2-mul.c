e92e08a56f8bf380f61886d17392aed708d660ea
anyadikeifeanyi98 <92559840+anyadikeifeanyi98@users.noreply.github.com>
2023-03-25 01:08:59 +0100

    Create 2-mul.c
    function to multipky 2 intergers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() 
+
+   {
+
+	int x, y, result;    printf("\nEnter the first integer: "); 
+
+    scanf("%d", &x);
+
+    printf("\nEnter the second integer: ");
+
+    scanf("%d", &y);
+
+    result = x * y;
+
+	printf("Product of the two integers = %d\n", result);
+
+}
